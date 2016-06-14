ba67a0267f4fb0e76f2f7e104c30a29d869a807e
CHANDRABHATLA RAGHUVAMSI <raghuvamsi.chandrabhatla@gmail.com>
2016-06-14 08:01:26 +0530

    Added topics basic template and directive for Modal with event handler flow. Need to add actual flow

--- app/src/js/forms/topics_controller.js
+++ app/src/js/forms/topics_controller.js
@@ -1,13 +1,45 @@
 app.controller("topicsController", ["$scope", "servicecalls",  function($scope, servicecalls) {
 
 	function onSuccess(res) {
-		debugger;
+		$scope.got_topics = true;
+		if(!res.data.length) {
+			$scope.no_topics = true;
+		} else {
+			// Have to add
+		}
 	}
 
 	function onError(res) {
-		debugger;
 	}
 
 
 	servicecalls.requestService("get_topics", {}).then(onSuccess, onError);
+
+	function switchModal() {
+		$scope.showModal = !$scope.showModal;
+	}
+
+	$scope.switchModal = switchModal;
 }]);
+
+
+app.directive("modal", function() {
+	return {
+		templateUrl: "/src/js/forms/modalDirective.html",
+		restrict: 'E',
+        transclude: true,
+        replace:true,
+        scope:true,
+        link: function postLink(scope, element, attrs) {
+        	scope.title = attrs.title;
+        	scope.saveBtn = attrs.savebtn;
+        	scope.$watch(attrs.show, function(show) {
+        		if(show) {
+        			$(element).modal("show");
+        		} else {
+        			$(element).modal("hide");
+        		}
+        	});
+        }
+	}
+});
